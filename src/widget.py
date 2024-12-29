4e456362fd8319e43d0f0b74a48bcd324005964a
Roman Siyanko <romasi98@mail.ru>
2024-12-30 00:18:21 +0300

    finally correct func

--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,10 @@
 from typing import Union
+from typing import Any
 from src.masks import get_mask_card_number
 from src.masks import get_mask_account
 
 
-def mask_account_card(numbers: Union[str, int]) -> Union[str, int]:
+def mask_account_card(numbers: Any) -> Any:
     """Создаем новую функцию, которая также макскирует номер, но теперь с названием счета (карты)"""
     new_numbers = numbers.split(' ')
     if 'Счет' in numbers:
@@ -12,13 +13,7 @@ def mask_account_card(numbers: Union[str, int]) -> Union[str, int]:
         return f'{' '.join(new_numbers[0:-1])} {get_mask_card_number(new_numbers[-1])}'
 
 
-
-
-def get_date(date:Union[int]) -> Union [str, int]:
+def get_date(date: Union[str]) -> Union[str, int]:
     """Создаем функцию, которая возвращает дату в'ДД.ММ.ГГГГ'"""
 
-    return f'"ДД.ММ.ГГГГ" ( "{date[5:7]}.{date[8:10]}.{date[0:4]}" )'
-
-
-
-
+    return f'ДД.ММ.ГГГГ ( {date[5:7]}.{date[8:10]}.{date[0:4]} )'
