6de7521968f187a06169d6fde7fe77b19f583700
Roman Siyanko <romasi98@mail.ru>
2024-12-29 16:35:35 +0300

    old functions

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+from typing import Union
+import os
+
+
+def get_mask_card_number(number: Union[int]) -> Union[str]:
+    """ Создаем функцию, которая принимает номер карты и возвращает маску номера карты"""
+    str_number = str(number)
+    return f'{str_number[0:4]} {str_number[4:6]}** **** {str_number[12:16]}'
+
+
+def get_mask_account(number_score: Union[int]) -> Union[str]:
+    """Создаем функцию, которая принимает номер счет и возвращает маску номера счета"""
+    str_number_score = str(number_score)
+    return f'**{str_number_score[-4:]}'
