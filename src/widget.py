eb84ec99b6d55d9f2b8229115a2aa1ccba4b18cd
Roman Siyanko <romasi98@mail.ru>
2024-12-29 23:47:37 +0300

    finall function

---
+++ src/widget.py
@@ -0,0 +1,24 @@
+from typing import Union
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+
+def mask_account_card(numbers: Union[str, int]) -> Union[str, int]:
+    """Создаем новую функцию, которая также макскирует номер, но теперь с названием счета (карты)"""
+    new_numbers = numbers.split(' ')
+    if 'Счет' in numbers:
+        return f'Счет {get_mask_account(numbers)}'
+    else:
+        return f'{' '.join(new_numbers[0:-1])} {get_mask_card_number(new_numbers[-1])}'
+
+
+
+
+def get_date(date:Union[int]) -> Union [str, int]:
+    """Создаем функцию, которая возвращает дату в'ДД.ММ.ГГГГ'"""
+
+    return f'"ДД.ММ.ГГГГ" ( "{date[5:7]}.{date[8:10]}.{date[0:4]}" )'
+
+
+
+
