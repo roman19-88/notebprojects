d533393c84ca9f16c5baa8ea090a736522740a9f
Roman Siyanko <romasi98@mail.ru>
2025-01-29 20:04:27 +0300

    fdf

--- src/processing.py
+++ src/processing.py
@@ -1,27 +1,19 @@
+from typing import Any
 
 
-
-def filter_by_state(list_dictionary):
+def filter_by_state(list_dictionary: Any) -> Any:
+    """ Функция сортирующая список словарей по 'EXECUTED'"""
     dict_state = []
     for i in list_dictionary:
-         if i['state']=='EXECUTED':
-             dict_state.append(i)
-
+        if i['state'] == 'EXECUTED':
+            dict_state.append(i)
     return dict_state
 
 
-def sort_by_date(data, descending=True):
+def sort_by_date(data: Any) -> Any:
+    """Функция сортирующая дату"""
+
     def data_get(item):
         return item['date']
 
-    return sorted(data, key=data_get ,reverse=descending)
-
-
-
-
-
-
-
-
-
-
+    return sorted(data, key=data_get, reverse=True)
