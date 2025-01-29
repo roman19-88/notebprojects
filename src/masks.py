ab3d77298548ef3a8ed857807e4fc7cccd162abf
Roman Siyanko <romasi98@mail.ru>
2025-01-29 15:38:56 +0300

    slihenie

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,16 @@
 from typing import Union
 
 
-
 def get_mask_card_number(number: Union[int]) -> Union[str]:
-    """ Создаем функцию, которая принимает номер карты и возвращает маску номера карты"""
+    """Создаем функцию, которая принимает номер карты и возвращает маску номера карты"""
     str_number = str(number)
-    return f'{str_number[0:4]} {str_number[4:6]}** **** {str_number[12:16]}'
+    return f"{str_number[0:4]} {str_number[4:6]}** **** {str_number[12:16]}"
 
 
 def get_mask_account(number_score: Union[int]) -> Union[str]:
     """Создаем функцию, которая принимает номер счет и возвращает маску номера счета"""
     str_number_score = str(number_score)
-    return f'**{str_number_score[-4:]}'
+    return f"**{str_number_score[-4:]}"
+
+x=3+5
+print(x)
