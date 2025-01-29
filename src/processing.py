3045074648caa59dec32b7a8eb401d02afc2d249
roman19-88 <romasi98@mail.ru>
2025-01-29 18:53:33 +0300

    Delete src/processing.py

--- src/processing.py
+++
@@ -1,8 +0,0 @@
-def filter_by_state(list_dictionary):
-    dict_state = []
-    for i in list_dictionary:
-         if i['state']=='EXECUTED':
-             dict_state.append(i)
-
-    return dict_state
-
