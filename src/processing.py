88df11edec89aff84ffd60ec1bfdb614a15e83ef
Roman Siyanko <romasi98@mail.ru>
2025-02-03 21:36:51 +0300

    домаш

--- src/processing.py
+++ src/processing.py
@@ -1,23 +1,20 @@
-from typing import Any, Iterable
+from typing import Iterable
 
-
-def filter_by_state(list_dictionary: Iterable[str], state_word: str = "EXECUTED") -> None:
+def filter_by_state(list_dictionary: list[dict], state_word: str = "EXECUTED") -> list[dict]:
     """Функция перебирает список словарей по ключу"""
 
     dict_state = []
-
     for elem in list_dictionary:
-        if elem["state"] == state_word:
+        if elem['state'] == state_word:
             dict_state.append(elem)
-
     return dict_state
 
 
-def sort_by_date(data: Iterable[str]) -> None:
+def sort_by_date(data: Iterable[dict], reverse_order = True ) -> list[dict]:
     """Функция сортирует по дате"""
 
-    def data_get(item: Any) -> Any:
+    def data_get(item: dict) -> str:
         """Функция вспомогающая, для сортировки"""
         return item["date"]
 
-    return sorted(data, key=data_get, reverse=True)
+    return sorted(data, key=data_get, reverse=reverse_order)
