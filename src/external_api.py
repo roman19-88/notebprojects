d65040072e3e844ad067b281dd7c1fc8f9cd0384
Roman Siyanko <romasi98@mail.ru>
2025-02-16 22:10:55 +0300

    logs

--- src/external_api.py
+++ src/external_api.py
@@ -1,7 +1,6 @@
 import requests
-API_KEY = 'bd047addbf603cdd539fc2619922cff5'
 
-
+API_KEY = 'b5293d52a016f02ea9adc610f43d8076'
 
 
 def convert_currency(transactions):
@@ -10,40 +9,38 @@ def convert_currency(transactions):
 
     for i in transactions:
         exchange_transaction = []
-        currency_code = i.get('operationAmount',{}).get('currency',{}).get('code')
+        currency_code = i.get('operationAmount', {}).get('currency', {}).get('code')
         amount = float(i.get('operationAmount', {}).get('amount'))
 
         if currency_code == 'RUB':
-            exchange_transaction.append(amount)
+            return float(amount)
 
-        params = {'access_key': API_KEY,
-                  'amount': amount,
-                  'from': currency_code,
-                  'to': 'RUB'}
-
+        print(currency_code)
+        params = {'access_key': API_KEY, 'from': currency_code, 'to': 'RUB', 'amount': amount}
         result = requests.get(url, params=params)
-
+        print(result.url)
+        print(result.status_code)
         if result.status_code == 200:
             data = result.json()
-
+            print(data)
             if 'rates' in data and 'RUB' in data['rates']:
                 exchange_rate = data['rates']['RUB'] * amount
                 exchange_transaction.append(exchange_rate)
     return exchange_transaction
 
 
-print(convert_currency( [{
+print(convert_currency([{
     "id": 41428829,
     "state": "EXECUTED",
     "date": "2019-07-03T18:35:29.512364",
     "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
+        "amount": "8221.37",
+        "currency": {
+            "name": 'USD',
+            "code": 'USD'
+        }
     },
     "description": "Перевод организации",
     "from": "MasterCard 7158300734726758",
     "to": "Счет 35383033474447895560"
-  }]))
+}]))
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,39 @@
 import json
+import logging
 import os
+
+dir_name_log = os.path.dirname(__file__)
+file_path_log = os.path.join(dir_name_log, '..', 'logs', 'utils.log')
+logger = logging.getLogger('utils')
+file_handler = logging.FileHandler(file_path_log, encoding='utf-8', mode='w')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def operations_finances(file_name):
     """функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
+    logger.info('Принял путь до нужного JSON файла')
+
     dir_name = os.path.dirname(__file__)
     file_path = os.path.join(dir_name, '..', 'data', file_name)
+
     try:
         with open(file_path, encoding='utf-8') as file:
             data = json.load(file)
-
+            logger.info('Открыл файл')
             if len(data) == 0:
+                logger.warning('Нет данных в файле')
                 return []
+
+            logger.info('Вернул список словарей с данными о финансах')
             return data
-    except (json.JSONDecodeError) as error:
-        print(f"Ошибка при чтении файла: {error}")
+
+    except json.JSONDecodeError as error:
+        logger.error(f"Ошибка при чтении файла: {error}", exc_info=True)
         return []
+
+
+if __name__ == "__main__":
+    operations_finances('operations.json')
